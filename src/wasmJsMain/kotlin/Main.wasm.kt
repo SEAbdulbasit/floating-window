aec63c55d89c6e120c2b65429d1495d58071baf4
Abdul Basit <abdulbasit5361234@gmail.com>
2023-11-25 14:59:08 +0500

    working prototype

--- src/wasmJsMain/kotlin/Main.wasm.kt
+++ src/wasmJsMain/kotlin/Main.wasm.kt
@@ -1,81 +1,150 @@
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.CanvasBasedWindow
+import kotlinx.browser.window
 
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     CanvasBasedWindow("Floating Window") {
+//        val canvasRef = remember { mutableStateOf(document.createElement("canvas") as HTMLCanvasElement?) }
+//        val coroutineScope = rememberCoroutineScope()
+//        val pElemText = remember { mutableStateOf("Window.screenLeft: 0, Window.screenTop: 0") }
+//        val position = remember { mutableStateOf(Offset.Zero) }
+//
+//        fun positionElem(timestamp: Double) {
+//            pElemText.value = "Window.screenLeft: ${window.screenX}, Window.screenTop: ${window.screenY}"
+//            position.value = Offset(window.screenX.toFloat(), window.screenY.toFloat())
+//
+//            window.requestAnimationFrame(::positionElem)
+//        }
+//        window.requestAnimationFrame(::positionElem)
+//
+//
+//        Text(pElemText.value)
+//
+//        androidx.compose.foundation.Canvas(modifier = Modifier) {
+//            drawCircle(
+//                color = Color.Red,
+//                center = Offset(window.screen.width / 2f, window.screen.height / 2f),
+//                radius = 50f
+//            )
+//        }
+//        val windowSize = IntSize(640, 480) // Set window size
+//
+//        val canvasSize = 400f // Canvas size
+//
+//        val circleRadius = 50f // Circle radius
+//        val circleColor = Color.Blue // Circle color
+//
+//        val initialLeft = remember { mutableStateOf(0f) }
+//        val initialTop = remember { mutableStateOf(0f) }
+//
+//        val screenLeft = remember { mutableStateOf(0f) }
+//        val screenTop = remember { mutableStateOf(0f) }
+//
+//        val position = remember { mutableStateOf(Offset.Zero) }
+//
+//        fun positionElem(timestamp: Double) {
+//            //   pElemText.value = "Window.screenLeft: ${window.screenX}, Window.screenTop: ${window.screenY}"
+//            //position.value = Offset(window.screenX.toFloat(), window.screenY.toFloat())
+//            initialLeft.value = window.screenX.toFloat()
+//            initialTop.value = window.screenY.toFloat()
+//
+//            window.requestAnimationFrame(::positionElem)
+//        }
+//        window.requestAnimationFrame(::positionElem)
+//
+//        Box(modifier = Modifier.fillMaxSize()) {
+//            Canvas(
+//                modifier = Modifier.align(Alignment.Center).fill.size(640.dp, 480.dp),
+//                onDraw = {
+//                    val width = size.width
+//                    val height = size.height
+//
+//                    if (initialLeft.value == 0f) {
+//                        initialLeft.value = window.screenX.toFloat()
+//                        initialTop.value = window.screenY.toFloat()
+//                    }
+//
+//                    screenLeft.value = window.screenX.toFloat()
+//                    screenTop.value = window.screenY.toFloat()
+//
+//                    val leftUpdate = initialLeft.value - screenLeft.value
+//                    val topUpdate = initialTop.value - screenTop.value
+//
+//                    drawRect(color = Color.Black, size = Size(640f, 480f))
+//                    drawCircle(
+//                        color = circleColor,
+//                        radius = circleRadius,
+//                        center = Offset(
+//                            (window.screen.width / 2).toLong().toFloat(),
+//                            (window.screen.height / 2).toLong().toFloat()
+//                        )
+//                    )
+//                }
+//            )
+//        }
+//
+//
+//        Text("Window.screenLeft: ${screenLeft.value.toInt()}, Window.screenTop: ${screenTop.value.toInt()}")
 
-        MaterialTheme {
-            val screenWidth = 640.dp
-            val screenHeight = 480.dp
 
-            val initialLeft = 0.dp
-            val initialTop = 0.dp
+        val windowSize = IntSize(640, 480) // Set window size
 
-            val leftUpdate = remember { mutableStateOf(initialLeft) }
-            val topUpdate = remember { mutableStateOf(initialTop) }
 
-            val density = LocalDensity.current.density
+        val canvasSize = 400f // Canvas size
+        val circleRadius = 50f // Circle radius
+        val circleColor = Color.Blue // Circle color
 
-            DisposableEffect(Unit) {
-                val animation = this. {
-                    while (true) {
-                        // Simulating screenLeft and screenTop changes
-                        leftUpdate.value += 1.dp
-                        topUpdate.value += 1.dp
-                        delay(16) // ~60 FPS
-                    }
-                }
-                onDispose {
-                    animation.cancel()
-                }
-            }
+        val initialLeft = remember { mutableStateOf(0f) }
+        val initialTop = remember { mutableStateOf(0f) }
 
-            val observedLeft by leftUpdate
-            val observedTop by topUpdate
+        val screenLeft = remember { mutableStateOf(0f) }
+        val screenTop = remember { mutableStateOf(0f) }
 
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Canvas(
-                    modifier = Modifier.size(screenWidth, screenHeight)
-                ) {
-                    drawIntoCanvas { canvas ->
-                        val width = size.width
-                        val height = size.height
-
-                        val leftUpdateValue = observedLeft.toPx()
-                        val topUpdateValue = observedTop.toPx()
-
-                        canvas.drawCircle(
-                            center = Offset(
-                                leftUpdateValue + (width / 2),
-                                topUpdateValue + (height / 2) + 35 * density
-                            ),
-                            radius = 50 * density,
-                            paint = Paint()
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-                Text("Window.screenLeft: $observedLeft, Window.screenTop: $observedTop")
-            }
+        fun positionElem(timestamp: Double) {
+            screenLeft.value = window.screenX.toFloat()
+            screenTop.value = window.screenY.toFloat()
+            window.requestAnimationFrame(::positionElem)
         }
+        window.requestAnimationFrame(::positionElem)
+
+        Canvas(
+            modifier = Modifier.size(480.dp, 680.dp),
+            onDraw = {
+                val width = size.width.toFloat()
+                val height = size.height.toFloat()
+
+                if (initialLeft.value == 0f) {
+                    initialLeft.value = window.screenX.toFloat()
+                    initialTop.value = window.screenY.toFloat()
+                }
+
+                val leftUpdate = initialLeft.value - screenLeft.value
+                val topUpdate = initialTop.value - screenTop.value
+
+                drawRect(color = Color.Black, size = Size(width, height))
+                drawCircle(
+                    color = circleColor,
+                    radius = circleRadius,
+                    center = Offset(leftUpdate + (width / 2), topUpdate + (height / 2) + 35)
+                )
+            }
+        )
+
+        Text("Window.screenLeft: ${screenLeft.value.toInt()}, Window.screenTop: ${screenTop.value.toInt()}")
     }
 }
+
 
