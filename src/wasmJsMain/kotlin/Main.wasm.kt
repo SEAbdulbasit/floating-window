a0fdfb366c16915ab09e6b57d91925558a507625
Abdul Basit <abdulbasit5361234@gmail.com>
2023-11-26 14:39:36 +0500

    add clip rect on circle

--- src/wasmJsMain/kotlin/Main.wasm.kt
+++ src/wasmJsMain/kotlin/Main.wasm.kt
@@ -1,13 +1,17 @@
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.CanvasBasedWindow
 import kotlinx.browser.window
@@ -31,9 +35,8 @@ fun main() {
             window.requestAnimationFrame(::positionElem)
         }
         window.requestAnimationFrame(::positionElem)
-
         Canvas(
-            modifier = Modifier.size(480.dp, 680.dp),
+            modifier = Modifier.size(680.dp, 480.dp),
             onDraw = {
                 val width = size.width.toFloat()
                 val height = size.height.toFloat()
@@ -47,13 +50,21 @@ fun main() {
                 val topUpdate = initialTop.value - screenTop.value
 
                 drawRect(color = Color.Black, size = Size(width, height))
-                drawCircle(
-                    color = circleColor,
-                    radius = circleRadius,
-                    center = Offset(leftUpdate + (width / 2), topUpdate + (height / 2) + 35)
-                )
+                clipRect(
+                    left = 0f,
+                    top = 0f,
+                    right = width,
+                    bottom = height
+                ) {
+                    drawCircle(
+                        color = circleColor,
+                        radius = circleRadius,
+                        center = Offset(leftUpdate + (width / 2), topUpdate + (height / 2) + 35)
+                    )
+                }
             }
         )
+
 
         Text("Window.screenLeft: ${screenLeft.value.toInt()}, Window.screenTop: ${screenTop.value.toInt()}")
     }
